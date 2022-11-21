760919708e1374837bd30770cb8e48608b1e5235
dhirajbennadi <89832913+dhirajbennadi@users.noreply.github.com>
2022-11-20 17:23:15 -0700

    First commit to check if Yocto Setup is working

---
+++ mqttApp.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include "MQTTClient.h"
+
+
+int main()
+{
+    printf("Hello Dhiraj Bennadi\n");
+
+    return 0;
+}
