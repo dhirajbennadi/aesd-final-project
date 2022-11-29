34f1f8d47093752b285e53ccfde582714af3e37a
dhirajbennadi <89832913+dhirajbennadi@users.noreply.github.com>
2022-11-29 01:16:34 -0700

    NTP Start Up script removed

--- ntp/ntp-start.sh
+++ ntp/ntp-start.sh
@@ -2,7 +2,7 @@
 
 echo "ntp_init script is statred..."
 
-cp /usr/bin/ntp.conf /etc/ntp.conf
+#cp /usr/bin/ntp.conf /etc/ntp.conf
 
 echo "ntp_init successfully updated ntp.conf to synchronize time with NTP server"
 
